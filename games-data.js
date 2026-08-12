93c083168e9270d29227bc284a2603feb8d3f305
YZer1990 <f8649480@gmail.com>
2026-08-12 12:32:15 +0800

    0812_01

---
+++ games-data.js
@@ -0,0 +1,212 @@
+// 遊戲庫資料（由 games.csv 產生）。真圖之後用同檔名覆蓋 assets/<slug>_cover.png / _icon.png 即可自動換上。
+window.GAMES = [
+ {
+  "slug": "hao-yun-cai-shen",
+  "name": "Hao Yun Cai Shen",
+  "category": "landbase",
+  "volatility": "High",
+  "lines": "50 Lines",
+  "rtp": "96.5%",
+  "maxMultiplier": "None",
+  "extraBet": "None",
+  "buyFeature": "50x",
+  "order": 1,
+  "media": {
+   "cover": "assets/hao-yun-cai-shen/hao-yun-cai-shen_cover.png",
+   "icon": "assets/hao-yun-cai-shen/hao-yun-cai-shen_icon.png"
+  }
+ },
+ {
+  "slug": "jin-shi-na-fu",
+  "name": "Jin Shi Na Fu",
+  "category": "landbase",
+  "volatility": "High",
+  "lines": "50 Lines",
+  "rtp": "96.5%",
+  "maxMultiplier": "None",
+  "extraBet": "None",
+  "buyFeature": "50x",
+  "order": 2,
+  "media": {
+   "cover": "assets/jin-shi-na-fu/jin-shi-na-fu_cover.png",
+   "icon": "assets/jin-shi-na-fu/jin-shi-na-fu_icon.png"
+  }
+ },
+ {
+  "slug": "jin-long-ying-cai",
+  "name": "Jin Long Ying Cai",
+  "category": "landbase",
+  "volatility": "High",
+  "lines": "50 Lines",
+  "rtp": "96.5%",
+  "maxMultiplier": "None",
+  "extraBet": "None",
+  "buyFeature": "50x",
+  "order": 3,
+  "media": {
+   "cover": "assets/jin-long-ying-cai/jin-long-ying-cai_cover.png",
+   "icon": "assets/jin-long-ying-cai/jin-long-ying-cai_icon.png"
+  }
+ },
+ {
+  "slug": "rong-hua-fu-gui",
+  "name": "Rong Hua Fu Gui",
+  "category": "landbase",
+  "volatility": "High",
+  "lines": "50 Lines",
+  "rtp": "96.5%",
+  "maxMultiplier": "None",
+  "extraBet": "None",
+  "buyFeature": "50x",
+  "order": 4,
+  "media": {
+   "cover": "assets/rong-hua-fu-gui/rong-hua-fu-gui_cover.png",
+   "icon": "assets/rong-hua-fu-gui/rong-hua-fu-gui_icon.png"
+  }
+ },
+ {
+  "slug": "super-chess",
+  "name": "Super Chess",
+  "category": "igaming",
+  "volatility": "High",
+  "lines": "1024 Ways",
+  "rtp": "96%",
+  "maxMultiplier": "None",
+  "extraBet": "None",
+  "buyFeature": "40.5x",
+  "order": 5,
+  "media": {
+   "cover": "assets/super-chess/super-chess_cover.png",
+   "icon": "assets/super-chess/super-chess_icon.png"
+  }
+ },
+ {
+  "slug": "sugar-pop",
+  "name": "Sugar Pop",
+  "category": "igaming",
+  "volatility": "Medium",
+  "lines": "25 Lines",
+  "rtp": "95.5%",
+  "maxMultiplier": "10000X",
+  "extraBet": "1.5x",
+  "buyFeature": "None",
+  "order": 6,
+  "media": {
+   "cover": "assets/sugar-pop/sugar-pop_cover.png",
+   "icon": "assets/sugar-pop/sugar-pop_icon.png"
+  }
+ },
+ {
+  "slug": "huang-chao-sheng-shi",
+  "name": "Huang Chao Sheng Shi",
+  "category": "landbase",
+  "volatility": "Medium",
+  "lines": "243 Ways",
+  "rtp": "",
+  "maxMultiplier": "None",
+  "extraBet": "None",
+  "buyFeature": "50x",
+  "order": 7,
+  "media": {
+   "cover": "assets/huang-chao-sheng-shi/huang-chao-sheng-shi_cover.png",
+   "icon": "assets/huang-chao-sheng-shi/huang-chao-sheng-shi_icon.png"
+  }
+ },
+ {
+  "slug": "huang-chao-mei-ren",
+  "name": "Huang Chao Mei Ren",
+  "category": "landbase",
+  "volatility": "Medium",
+  "lines": "243 Ways",
+  "rtp": "",
+  "maxMultiplier": "None",
+  "extraBet": "None",
+  "buyFeature": "50x",
+  "order": 8,
+  "media": {
+   "cover": "assets/huang-chao-mei-ren/huang-chao-mei-ren_cover.png",
+   "icon": "assets/huang-chao-mei-ren/huang-chao-mei-ren_icon.png"
+  }
+ },
+ {
+  "slug": "witchs-magic",
+  "name": "Witch's Magic",
+  "category": "igaming",
+  "volatility": "High",
+  "lines": "3600 Ways",
+  "rtp": "96%",
+  "maxMultiplier": "10000X",
+  "extraBet": "1.5x",
+  "buyFeature": "75x / 120x",
+  "order": 9,
+  "media": {
+   "cover": "assets/witchs-magic/witchs-magic_cover.png",
+   "icon": "assets/witchs-magic/witchs-magic_icon.png"
+  }
+ },
+ {
+  "slug": "jin-feng-fei-wu",
+  "name": "Jin Feng Fei Wu",
+  "category": "landbase",
+  "volatility": "High",
+  "lines": "243 Ways",
+  "rtp": "96.5%",
+  "maxMultiplier": "None",
+  "extraBet": "None",
+  "buyFeature": "50x",
+  "order": 10,
+  "media": {
+   "cover": "assets/jin-feng-fei-wu/jin-feng-fei-wu_cover.png",
+   "icon": "assets/jin-feng-fei-wu/jin-feng-fei-wu_icon.png"
+  }
+ },
+ {
+  "slug": "hua-hao-yue-yuan",
+  "name": "Hua Hao Yue Yuan",
+  "category": "landbase",
+  "volatility": "High",
+  "lines": "243 Ways",
+  "rtp": "96.5%",
+  "maxMultiplier": "None",
+  "extraBet": "None",
+  "buyFeature": "50x",
+  "order": 11,
+  "media": {
+   "cover": "assets/hua-hao-yue-yuan/hua-hao-yue-yuan_cover.png",
+   "icon": "assets/hua-hao-yue-yuan/hua-hao-yue-yuan_icon.png"
+  }
+ },
+ {
+  "slug": "bathala-1000",
+  "name": "Bathala 1000",
+  "category": "igaming",
+  "volatility": "Medium",
+  "lines": "5 Lines",
+  "rtp": "96%",
+  "maxMultiplier": "30000X",
+  "extraBet": "1.5x",
+  "buyFeature": "None",
+  "order": 12,
+  "detailUrl": "games/bathala-1000/",
+  "media": {
+   "cover": "games/bathala-1000/images/bathala-1000_cover.png",
+   "icon": "games/bathala-1000/images/bathala-1000_icon.png"
+  }
+ },
+ {
+  "slug": "funky-ape",
+  "name": "Funky Ape",
+  "category": "igaming",
+  "volatility": "High",
+  "lines": "256-32400 Ways",
+  "rtp": "",
+  "maxMultiplier": "10000X",
+  "extraBet": "1.5x / 2x / 3x",
+  "buyFeature": "",
+  "order": 13,
+  "media": {
+   "cover": "assets/funky-ape/funky-ape_cover.png",
+   "icon": "assets/funky-ape/funky-ape_icon.png"
+  }
+ }
+];
